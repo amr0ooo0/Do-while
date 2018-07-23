b0292d932cf340307416b226f907ff063057af7f
amr mohamed elsayed <Amr0ooo0@outlook.com>
2018-07-23 17:27:28 +0200

    Add project files.

---
+++ Do while/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  mySalary = 1000 , incresment = 20, tagetsalary = 2000;
+            int i = 0;
+
+            do
+
+            {
+                mySalary += incresment;
+                i++;
+            }
+            while (mySalary < tagetsalary);
+
+            float years = i / 12;
+            Console.WriteLine("{0}\n{1}", i, years);
+
+
+            Console.ReadKey();
+
+
+            
+
+
+
+          
+
+        }
+    }
+}
